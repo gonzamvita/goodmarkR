f2e7f46d2f5b7b93a950b16ffd5a8f499a5b5580
gonzamvita <gonzamv@gmail.com>
2015-12-18 00:26:45 +0100

    Styles

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,6 +2,6 @@ class BookmarksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @bookmarks = Bookmark.where(user: current_user)
+    @bookmarks = Bookmark.where(user: current_user).order(created_at: :desc)
   end
 end
