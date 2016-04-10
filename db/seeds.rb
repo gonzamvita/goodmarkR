3a44e856031d720a94a110863724a9a5b1dd1222
gonzamvita <gonzamv@gmail.com>
2016-04-10 16:31:51 +0200

    adding seeds for bookmark types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+BookmarkType.create(description: 'New')
+BookmarkType.create(description: 'Image')
+BookmarkType.create(description: 'General')
